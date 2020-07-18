27ff8f9e454fa0af0562d47b7bf6c63e071f5abf
Abdulhadi Masri <abdul.hadi.95@hotmail.com>
2020-07-18 16:30:41 +0200

    IRepository interface added

---
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    //it's a generic Interface 
+    //generics allow you to write a class or method that can work with any data type.
+    public interface IRepository<T> where T:class
+    {
+        T Get(int id);
+
+
+        //IEnumerable in C# is an interface that defines one method
+        //This allows readonly access to a collection then a collection that implements IEnumerable can be used with a for-each statement.
+        IEnumerable<T> GetAll(
+            Expression<Func<T,bool>> filter = null,
+            Func<IQueryable<T>,IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+
+        T GetFirstOrDefault(
+      Expression<Func<T, bool>> filter = null,
+      string includeProperties = null
+      );
+
+        void Add(T entity);
+        void Remove(int id);
+
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
